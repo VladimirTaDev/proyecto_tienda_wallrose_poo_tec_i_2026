9641a5ee898af481c42de5695301b09452b5a918
Vladimir Tarassenko <99820518+VladimirTaDev@users.noreply.github.com>
2026-05-13 15:43:02 -0600

    Expandir Cliente con datos básicos y gestión de órdenes
    Expandir la clase Cliente: agregar los atributos id, email, nombre y un TreeMap<Integer, OrdenCompra> ordenes. Proporcionar un constructor para inicializar los atributos y ordenes, getters/setters para id/email/nombre y acceso a ordenes. Agregar métodos auxiliares agregarOrden y borrarOrden que almacenan/eliminan OrdenCompra usando su numero. También agregar imports de Map y TreeMap, y eliminar el constructor placeholder sin argumentos.

--- src/logica/Cliente.java
+++ src/logica/Cliente.java
@@ -1,9 +1,56 @@
 package logica;
 
-public class Cliente {
+import java.util.Map;
+import java.util.TreeMap;
 
-	public Cliente() {
-		// TODO Auto-generated constructor stub
+public class Cliente {
+	private String id;
+	private String email;
+	private String nombre;
+	private Map<Integer, OrdenCompra> ordenes;
+	
+
+	public Cliente(String id, String email, String nombre) {
+		this.id = id;
+		this.email = email;
+		this.nombre = nombre;
+		this.ordenes = new TreeMap<>();
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+	public Map<Integer, OrdenCompra> getOrdenes() {
+		return ordenes;
+	}
+	
+	public void agregarOrden(OrdenCompra orden) {
+		ordenes.put(orden.getNumero(), orden);
+	}
+	
+	public void borrarOrden(OrdenCompra orden) {
+		ordenes.remove(orden.getNumero());
+	}
+	
 }
